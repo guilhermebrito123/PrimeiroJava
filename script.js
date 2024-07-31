0ecd46a5a193a91ea1459af946ed07585363d549
guilhermebrito123 <gbfsilva@sga.pucminas.br>
2024-07-30 23:01:07 -0300

    Substituindo o documento perfil.html (excluindo-o), pois o sobrevc.html tem a mesma função de exibição dos dados cadastrados

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-//let nome = window.prompt('Digite seu nome')
-//window.alert(`É um prazer te conhecer ${nome}`)
+var nome = window.prompt('Digite seu nome')
+window.alert(`É um prazer te conhecer ${nome}`)
+
+document.getElementById('name').innerHTML = nome
+
 let valor1
 let valor2
 
@@ -23,3 +26,35 @@ let myCar = {
 for (let item in myCar){
     console.log(item, myCar[item])//Respectivamente imprimo quais as propriedades existentes dentro do objeto myCar e em seguida seus valores utilizando colchetes
 }
+
+function createBook(title, author, pages){
+    const book = {
+        bookTitle: title,
+        bookAuthor: author,
+        bookPages: pages,
+        printBook: function(){
+            console.log('Printing...')
+        }
+    }
+    return book
+}
+
+//FACTORY (FÁBRICA DE OBJETOS COM CARACTERÍSTICAS SEMELHANTES):
+const book1 = createBook("Shakes and pinks","Guilherme de Brito", 300)
+const book2 = createBook("Shakes and pinks 2","Guilherme de Brito", 450)
+
+book1.bookColor = 'White'
+
+
+console.log(book1)
+console.log(book2)
+
+//CONSTRUCTOR (MESMA FUNÇÃO DE FACTORY, PORÉM MAIS SIMPLES E MAIS UTILIZADO):
+function CreateBook(title, author, pages){
+    this.bookTitle = title;
+    this.bookAuthor = author;
+    this.bookPages = pages
+}
+
+const book3 = new CreateBook("Black Lives Matter", "Pedro Henrique Brito", 500)
+console.log(book3)
